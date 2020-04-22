ad7a810f326086e4a3775cb62737cd08fdf691aa
wyf0762 <57795957+wyf0762@users.noreply.github.com>
2020-04-22 13:25:43 +0800

    add config file

--- OnlineList.py
+++ OnlineList.py
@@ -3,7 +3,33 @@ import json
 Prefix = "!!online"
 PluginName = "OnlineList"
 DataPath = "plugins/" + PluginName + "/data.json"
+ConfigFilePath = "plugins/config/" + PluginName + ".json"
 data = []
+carpet_player = False
+start_with = []
+blacklist = []
+whitelist = []
+
+def load_config(server):
+    global carpet_player
+    global start_with
+    global blacklist
+    global whitelist
+    try:
+        with open(ConfigFilePath) as file:
+            # print("open ok")
+            config = json.load(file, encoding='utf8')
+            # print("json load ok")
+    except:
+        return
+
+    print(config)
+    carpet_player = config["carpet_player"]
+    start_with = config["start_with"]
+    blacklist = config["blacklist"]
+    whitelist = config["whitelist"]
+
+    print("[OnlineList] 配置文件加载成功")
 
 def load_data():
     global data
@@ -55,15 +81,24 @@ def get_online_list():
         ret.append(data[i]["name"])
     return ret
 
+def on_load(server, module):
+    server.add_help_message(Prefix, "获取在线玩家列表并自动识别bot")
+    load_config()
+
 def on_player_joined(server, player):
     global data
+    global carpet_player
+    global start_with
+    global blacklist
+    global whitelist
+    load_config()
     load_data()
     add_data(player, False)
     # print("[OnlineList]" + player + " joined the game")
 
 def on_player_left(server, player):
     global data
-    load_data()
+    load_data(server)
     delete_data(player)
     # print("[OnlineList]" + player + " left the game")
 
@@ -72,9 +107,13 @@ def on_info(server, info):
     splited_content = content.split()
     if splited_content[0] != Prefix:
         return
-    if len(splited_content) != 1:
+    if content == Prefix:
+        show_list(server, info)
+    if content == Prefix + " reload":
+        load_config(server)
+        server.say("[OnlineList] §a配置文件加载成功§r")
+    else:
         server.reply(info, "§c格式错误§r")
         return
-    show_list(server, info)
 
 
